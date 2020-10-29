19abb0a41db74aafea709eb45e76081e2b2ce24c
chandra <shekarchandru_5@rediffmail.com>
2020-10-29 10:33:36 +0530

    branch1 first commit

---
+++ EclipseProject/Project2/src/Customer.java
@@ -0,0 +1,11 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("We Have Added Customers Project to the Repo...");
+
+	}
+
+}
